ae89c86cb3f553ceff976e1c89ef8c962efda18b
Yash Swastik <yashswastik55@gmail.com>
2026-08-06 13:24:07 +0530

    Time: 195 ms (5.32%), Space: 11 MB (41.73%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.c
@@ -0,0 +1,15 @@
+int majorityElement(int* nums, int numsSize) {
+    int z = numsSize/2;
+    for (int i = 1; i < numsSize; i++) {
+    int key = nums[i];
+    int j = i - 1;
+
+    while (j >= 0 && nums[j] > key) {
+        nums[j + 1] = nums[j];
+        j--;
+    }
+
+    nums[j + 1] = key;
+    }
+    return nums[z];
+}
