eff7eb0523e6fcbddebecf2d1ca5d88a039f568a
Yash Swastik <yashswastik55@gmail.com>
2026-08-12 14:30:34 +0530

    Time: 0 ms (100%), Space: 10.2 MB (34.16%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.c
@@ -0,0 +1,25 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize) {
+    int *arr = malloc((digitsSize + 1) * sizeof(int));
+
+    for(int i = digitsSize - 1; i >= 0; i--) {
+        if(digits[i] != 9) {
+            digits[i]++;
+            *returnSize = digitsSize;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+
+    arr[0] = 1;
+
+    for(int i = 1; i <= digitsSize; i++) {
+        arr[i] = 0;
+    }
+
+    *returnSize = digitsSize + 1;
+    return arr;
+}
